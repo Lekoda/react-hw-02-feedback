cc4291a19c2de6d350a6db0f7e82eeb2a6aa251f
Valeria Otchenash <kratorchestra@gmail.com>
2023-08-22 14:16:23 +0300

    adds styled Statistics component

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,13 +1,16 @@
+import { Container } from './Statistics.styled';
+import { List } from './Statistics.styled';
+
 export const Statistics = ({ good, neutral, bad, total, positive }) => {
   return (
-    <>
-      <ul>
+    <Container>
+      <List>
         <li>Good: {good}</li>
         <li>Neutral: {neutral}</li>
         <li>Bad:{bad}</li>
         <li>Total: {total}</li>
         <li>positive feedback:{positive} %</li>
-      </ul>
-    </>
+      </List>
+    </Container>
   );
 };
---
+++ src/components/Statistics/Statistics.styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+export const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 0;
+  padding: 0;
+  padding-bottom: 20px;
+  list-style: none;
+  font-size: large;
+  color: #f5f8fa;
+`;
